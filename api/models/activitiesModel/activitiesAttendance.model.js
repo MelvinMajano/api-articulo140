0c6516725b9d20ebfdd8d10f9bf7bcc23313ecdf
WilmerH1506 <wyhernandezr@unah.hn>
2025-07-31 16:56:31 -0600

    Finalizacion de logica de asistencia y estandarizacion de uuidv4 y funcion now mysql

--- api/models/activitiesModel/activitiesAttendance.model.js
+++ api/models/activitiesModel/activitiesAttendance.model.js
@@ -1,16 +1,16 @@
 import { pool } from "../../config/db.js";
 
 export const registerAttendanceModel = async (data) => {
-  const { registro_id, hora_entrada, hora_salida } = data;
+  const {id, registro_id, hora_entrada, hora_salida } = data;
   const cnn = await pool.getConnection();
 
   try {
     await cnn.beginTransaction();
 
     const query = `insert into asistencia(id,registro_id,hora_entrada,hora_salida,created_at) 
-                   values (UUID(), ? , ? , ? , CURDATE())`;
+                   values (?, ? , ? , ? , NOW())`;
 
-    const [rows] = await cnn.execute(query, [registro_id, hora_entrada, hora_salida]);
+    const [rows] = await cnn.execute(query, [id, registro_id, hora_entrada, hora_salida]);
 
     cnn.commit();
     return rows;
@@ -22,3 +22,52 @@ export const registerAttendanceModel = async (data) => {
     cnn.release();
   }
 };
+
+export const getAttendanceModel = async (activityID) => {
+
+  const cnn = await pool.getConnection()
+
+  try{
+
+    const query = `select a.titulo,u.nombre, u.numero_cuenta ,asist.hora_entrada, asist.hora_salida 
+                   from actividad as a 
+                   inner join registro as r on a.id = r.actividad_id
+                   inner join users as u on u.id = r.estudiante_id
+                   left join asistencia as asist on asist.registro_id = r.id
+                   where a.id = ?`
+
+    const [rows] = await cnn.execute(query, [activityID])
+    return rows
+
+  } catch(error) {
+    throw error;
+  }
+}
+
+export const updateAttendanceModel = async (activityID, userID, data) => {
+
+  const { hora_entrada, hora_salida } = data;
+  const cnn = await pool.getConnection();
+
+  cnn.beginTransaction();
+
+  try{
+
+    const query = `update asistencia as a 
+                   join registro as r on a.registro_id = r.id
+                   join actividad as act on act.id = r.actividad_id
+                   set hora_entrada = ? , hora_salida = ?
+                   where act.id = ? and r.estudiante_id = ?`
+
+    const [rows] = await cnn.execute(query, [hora_entrada,hora_salida,activityID,userID])
+
+    await cnn.commit();
+    return rows;
+
+  } catch (error) {
+    cnn.rollback();
+    throw error;
+  } finally {
+    cnn.release();  
+  }
+}
--- api/models/activitiesModel/activitiesInscripciones.model.js
+++ api/models/activitiesModel/activitiesInscripciones.model.js
@@ -1,15 +1,15 @@
 import {pool} from "../../config/db.js"
 
-export const registerStudentModel = async (studentID, activityID) => {
+export const registerStudentModel = async (id,studentID, activityID) => {
 
     const cnn = await pool.getConnection();
     await cnn.beginTransaction();
 
     try  {
         const query = `insert into registro (id,actividad_id,estudiante_id,estado_registro_id, fecha_inscripcion)
-                       values (uuid(), ? , ? , 1, CURDATE())` 
+                       values (?, ? , ? , 1, NOW())` 
 
-        await cnn.execute(query, [activityID, studentID])
+        await cnn.execute(query, [id, activityID, studentID])
 
         cnn.commit();
         
